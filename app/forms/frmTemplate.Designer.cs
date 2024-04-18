d6844dc1565b2fd606ba97c75f9b74eb4a7044e4
Danilo <pokava2005@gmail.com>
2024-04-18 22:30:01 +0200

    fixed icon colors

--- app/forms/frmTemplate.Designer.cs
+++ app/forms/frmTemplate.Designer.cs
@@ -30,25 +30,25 @@
         {
             this.pnlSideBar = new System.Windows.Forms.Panel();
             this.pnlTop = new System.Windows.Forms.Panel();
-            this.pBoxUser = new System.Windows.Forms.PictureBox();
             this.panel5 = new System.Windows.Forms.Panel();
-            this.btnRacuni = new System.Windows.Forms.Button();
             this.panel7 = new System.Windows.Forms.Panel();
-            this.btnExit = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.btnDashboard = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.btnArtikli = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pBoxUser = new System.Windows.Forms.PictureBox();
+            this.btnRacuni = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.btnArtikli = new System.Windows.Forms.Button();
             this.btnZaposleni = new System.Windows.Forms.Button();
             this.pnlSideBar.SuspendLayout();
             this.pnlTop.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pBoxUser)).BeginInit();
             this.panel5.SuspendLayout();
             this.panel7.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxUser)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlSideBar
@@ -74,16 +74,6 @@
             this.pnlTop.Size = new System.Drawing.Size(174, 159);
             this.pnlTop.TabIndex = 8;
             // 
-            // pBoxUser
-            // 
-            this.pBoxUser.Image = global::butik.Properties.Resources.user_icon_64;
-            this.pBoxUser.InitialImage = null;
-            this.pBoxUser.Location = new System.Drawing.Point(52, 22);
-            this.pBoxUser.Name = "pBoxUser";
-            this.pBoxUser.Size = new System.Drawing.Size(67, 67);
-            this.pBoxUser.TabIndex = 7;
-            this.pBoxUser.TabStop = false;
-            // 
             // panel5
             // 
             this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
@@ -95,23 +85,6 @@
             this.panel5.Size = new System.Drawing.Size(151, 44);
             this.panel5.TabIndex = 4;
             // 
-            // btnRacuni
-            // 
-            this.btnRacuni.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(46)))), ((int)(((byte)(54)))));
-            this.btnRacuni.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
-            this.btnRacuni.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
-            this.btnRacuni.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnRacuni.Image = global::butik.Properties.Resources.bill_icon;
-            this.btnRacuni.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnRacuni.Location = new System.Drawing.Point(-3, -9);
-            this.btnRacuni.Name = "btnRacuni";
-            this.btnRacuni.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnRacuni.Size = new System.Drawing.Size(191, 62);
-            this.btnRacuni.TabIndex = 3;
-            this.btnRacuni.Text = "        Racuni";
-            this.btnRacuni.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnRacuni.UseVisualStyleBackColor = false;
-            // 
             // panel7
             // 
             this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
@@ -120,26 +93,6 @@
             this.panel7.Name = "panel7";
             this.panel7.Size = new System.Drawing.Size(151, 44);
             this.panel7.TabIndex = 6;
-            // 
-            // btnExit
-            // 
-            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(46)))), ((int)(((byte)(54)))));
-            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
-            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
-            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnExit.Image = global::butik.Properties.Resources.exit_icon;
-            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnExit.Location = new System.Drawing.Point(-3, -11);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnExit.Size = new System.Drawing.Size(191, 62);
-            this.btnExit.TabIndex = 3;
-            this.btnExit.Text = "        Izlaz";
-            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnExit.UseVisualStyleBackColor = false;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // panel3
             // 
@@ -152,26 +105,6 @@
             this.panel3.Size = new System.Drawing.Size(151, 44);
             this.panel3.TabIndex = 2;
             // 
-            // btnDashboard
-            // 
-            this.btnDashboard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(46)))), ((int)(((byte)(54)))));
-            this.btnDashboard.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.btnDashboard.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
-            this.btnDashboard.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
-            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDashboard.Image = global::butik.Properties.Resources.dashboard_icon;
-            this.btnDashboard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnDashboard.Location = new System.Drawing.Point(-3, -9);
-            this.btnDashboard.Name = "btnDashboard";
-            this.btnDashboard.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnDashboard.Size = new System.Drawing.Size(191, 62);
-            this.btnDashboard.TabIndex = 3;
-            this.btnDashboard.Text = "        Dashboard";
-            this.btnDashboard.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnDashboard.UseVisualStyleBackColor = false;
-            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
-            this.btnDashboard.Paint += new System.Windows.Forms.PaintEventHandler(this.btnDashboard_Paint);
-            // 
             // panel4
             // 
             this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
@@ -182,23 +115,6 @@
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(151, 44);
             this.panel4.TabIndex = 5;
-            // 
-            // btnArtikli
-            // 
-            this.btnArtikli.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(46)))), ((int)(((byte)(54)))));
-            this.btnArtikli.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
-            this.btnArtikli.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
-            this.btnArtikli.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnArtikli.Image = global::butik.Properties.Resources.artikl_icon;
-            this.btnArtikli.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnArtikli.Location = new System.Drawing.Point(-3, -9);
-            this.btnArtikli.Name = "btnArtikli";
-            this.btnArtikli.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnArtikli.Size = new System.Drawing.Size(191, 62);
-            this.btnArtikli.TabIndex = 3;
-            this.btnArtikli.Text = "        Artikli";
-            this.btnArtikli.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnArtikli.UseVisualStyleBackColor = false;
             // 
             // panel2
             // 
@@ -211,13 +127,97 @@
             this.panel2.Size = new System.Drawing.Size(151, 44);
             this.panel2.TabIndex = 4;
             // 
+            // pBoxUser
+            // 
+            this.pBoxUser.Image = global::butik.Properties.Resources.user_icon_64_white;
+            this.pBoxUser.InitialImage = null;
+            this.pBoxUser.Location = new System.Drawing.Point(52, 22);
+            this.pBoxUser.Name = "pBoxUser";
+            this.pBoxUser.Size = new System.Drawing.Size(67, 67);
+            this.pBoxUser.TabIndex = 7;
+            this.pBoxUser.TabStop = false;
+            // 
+            // btnRacuni
+            // 
+            this.btnRacuni.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(46)))), ((int)(((byte)(54)))));
+            this.btnRacuni.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
+            this.btnRacuni.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
+            this.btnRacuni.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRacuni.Image = global::butik.Properties.Resources.bill_icon_white;
+            this.btnRacuni.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRacuni.Location = new System.Drawing.Point(-3, -9);
+            this.btnRacuni.Name = "btnRacuni";
+            this.btnRacuni.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnRacuni.Size = new System.Drawing.Size(191, 62);
+            this.btnRacuni.TabIndex = 3;
+            this.btnRacuni.Text = "        Racuni";
+            this.btnRacuni.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRacuni.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(46)))), ((int)(((byte)(54)))));
+            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnExit.Image = global::butik.Properties.Resources.exit_icon_white;
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.Location = new System.Drawing.Point(-3, -11);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnExit.Size = new System.Drawing.Size(191, 62);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "        Izlaz";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(46)))), ((int)(((byte)(54)))));
+            this.btnDashboard.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnDashboard.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
+            this.btnDashboard.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Image = global::butik.Properties.Resources.dashboard_icon_white;
+            this.btnDashboard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDashboard.Location = new System.Drawing.Point(-3, -9);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnDashboard.Size = new System.Drawing.Size(191, 62);
+            this.btnDashboard.TabIndex = 3;
+            this.btnDashboard.Text = "        Dashboard";
+            this.btnDashboard.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDashboard.UseVisualStyleBackColor = false;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
+            this.btnDashboard.Paint += new System.Windows.Forms.PaintEventHandler(this.btnDashboard_Paint);
+            // 
+            // btnArtikli
+            // 
+            this.btnArtikli.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(46)))), ((int)(((byte)(54)))));
+            this.btnArtikli.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
+            this.btnArtikli.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
+            this.btnArtikli.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnArtikli.Image = global::butik.Properties.Resources.artikl_icon_white;
+            this.btnArtikli.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnArtikli.Location = new System.Drawing.Point(-3, -9);
+            this.btnArtikli.Name = "btnArtikli";
+            this.btnArtikli.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnArtikli.Size = new System.Drawing.Size(191, 62);
+            this.btnArtikli.TabIndex = 3;
+            this.btnArtikli.Text = "        Artikli";
+            this.btnArtikli.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnArtikli.UseVisualStyleBackColor = false;
+            // 
             // btnZaposleni
             // 
             this.btnZaposleni.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(46)))), ((int)(((byte)(54)))));
             this.btnZaposleni.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
             this.btnZaposleni.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
             this.btnZaposleni.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnZaposleni.Image = global::butik.Properties.Resources.zaposleni_icon;
+            this.btnZaposleni.Image = global::butik.Properties.Resources.zaposleni_icon_white;
             this.btnZaposleni.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnZaposleni.Location = new System.Drawing.Point(-3, -9);
             this.btnZaposleni.Name = "btnZaposleni";
@@ -242,12 +242,12 @@
             this.Text = "frmTemplate";
             this.pnlSideBar.ResumeLayout(false);
             this.pnlTop.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pBoxUser)).EndInit();
             this.panel5.ResumeLayout(false);
             this.panel7.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxUser)).EndInit();
             this.ResumeLayout(false);
 
         }
