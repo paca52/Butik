067a1751aa11c9c19c9a2cfa83eb351136fc674e
paca52 <pavlaka.np@gmail.com>
2024-04-30 18:11:32 +0200

    fixed artiklIndex form style

--- app/Properties/Settings.Designer.cs
+++ app/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace butik.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=PC08-K4;Initial Catalog=db_butik;Integrated Security=True;TrustServer" +
-            "Certificate=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost\\SQLEXPRESS;Initial Catalog=db_butik;Integrated Security=Tru" +
+            "e;TrustServerCertificate=True")]
         public string connStr {
             get {
                 return ((string)(this["connStr"]));
--- app/forms/artikli/frmArtikliDodaj.Designer.cs
+++ app/forms/artikli/frmArtikliDodaj.Designer.cs
@@ -36,122 +36,188 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.btnExit = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.tBoxDostavljenaKolicina = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(181, 74);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(87, 102);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.Size = new System.Drawing.Size(20, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "id";
+            this.label1.Text = "Id";
             // 
             // cBoxId
             // 
+            this.cBoxId.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
             this.cBoxId.FormattingEnabled = true;
-            this.cBoxId.Location = new System.Drawing.Point(184, 99);
+            this.cBoxId.Location = new System.Drawing.Point(90, 123);
             this.cBoxId.Name = "cBoxId";
-            this.cBoxId.Size = new System.Drawing.Size(184, 21);
+            this.cBoxId.Size = new System.Drawing.Size(532, 29);
             this.cBoxId.TabIndex = 1;
             this.cBoxId.SelectedIndexChanged += new System.EventHandler(this.cBoxId_SelectedIndexChanged);
             // 
             // tBoxNaziv
             // 
-            this.tBoxNaziv.Location = new System.Drawing.Point(184, 168);
+            this.tBoxNaziv.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.tBoxNaziv.Location = new System.Drawing.Point(90, 192);
             this.tBoxNaziv.Name = "tBoxNaziv";
-            this.tBoxNaziv.Size = new System.Drawing.Size(184, 20);
+            this.tBoxNaziv.Size = new System.Drawing.Size(532, 29);
             this.tBoxNaziv.TabIndex = 2;
             // 
             // tBoxKolicina
             // 
-            this.tBoxKolicina.Location = new System.Drawing.Point(184, 233);
+            this.tBoxKolicina.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.tBoxKolicina.Location = new System.Drawing.Point(91, 266);
             this.tBoxKolicina.Name = "tBoxKolicina";
-            this.tBoxKolicina.Size = new System.Drawing.Size(184, 20);
+            this.tBoxKolicina.Size = new System.Drawing.Size(258, 29);
             this.tBoxKolicina.TabIndex = 3;
             // 
             // tBoxCena
             // 
-            this.tBoxCena.Location = new System.Drawing.Point(184, 285);
+            this.tBoxCena.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.tBoxCena.Location = new System.Drawing.Point(90, 332);
             this.tBoxCena.Name = "tBoxCena";
-            this.tBoxCena.Size = new System.Drawing.Size(184, 20);
+            this.tBoxCena.Size = new System.Drawing.Size(532, 29);
             this.tBoxCena.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(181, 152);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Location = new System.Drawing.Point(87, 171);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.Size = new System.Drawing.Size(85, 17);
             this.label2.TabIndex = 5;
             this.label2.Text = "Naziv artikla";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(181, 217);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label3.Location = new System.Drawing.Point(87, 245);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.Size = new System.Drawing.Size(166, 17);
             this.label3.TabIndex = 6;
             this.label3.Text = "Dostupna Kolicina Artikla";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(181, 269);
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label4.Location = new System.Drawing.Point(87, 311);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.Size = new System.Drawing.Size(81, 17);
             this.label4.TabIndex = 7;
             this.label4.Text = "Cena artikla";
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Location = new System.Drawing.Point(494, 467);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(142, 37);
-            this.btnAdd.TabIndex = 8;
-            this.btnAdd.Text = "Dodaj";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-            // 
-            // btnExit
-            // 
-            this.btnExit.Location = new System.Drawing.Point(72, 467);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(142, 37);
-            this.btnExit.TabIndex = 9;
-            this.btnExit.Text = "Izlaz";
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(407, 217);
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label5.Location = new System.Drawing.Point(360, 245);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(135, 13);
+            this.label5.Size = new System.Drawing.Size(179, 17);
             this.label5.TabIndex = 11;
             this.label5.Text = "Dostavljena Kolicina Artikla";
             // 
             // tBoxDostavljenaKolicina
             // 
-            this.tBoxDostavljenaKolicina.Location = new System.Drawing.Point(410, 233);
+            this.tBoxDostavljenaKolicina.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.tBoxDostavljenaKolicina.Location = new System.Drawing.Point(364, 266);
             this.tBoxDostavljenaKolicina.Name = "tBoxDostavljenaKolicina";
-            this.tBoxDostavljenaKolicina.Size = new System.Drawing.Size(184, 20);
+            this.tBoxDostavljenaKolicina.Size = new System.Drawing.Size(258, 29);
             this.tBoxDostavljenaKolicina.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.btnAdd);
+            this.panel3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel3.Location = new System.Drawing.Point(474, 445);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(149, 44);
+            this.panel3.TabIndex = 12;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(46)))), ((int)(((byte)(54)))));
+            this.btnAdd.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
+            this.btnAdd.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdd.Location = new System.Drawing.Point(-3, -9);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnAdd.Size = new System.Drawing.Size(153, 62);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Dodaj";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Location = new System.Drawing.Point(92, 445);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(149, 44);
+            this.panel1.TabIndex = 6;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(46)))), ((int)(((byte)(54)))));
+            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(37)))), ((int)(((byte)(46)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.Location = new System.Drawing.Point(-3, -9);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnExit.Size = new System.Drawing.Size(153, 62);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Izlaz";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(83, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 37);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "ARTIKL";
             // 
             // frmArtikliDodaj
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.ClientSize = new System.Drawing.Size(709, 531);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.tBoxDostavljenaKolicina);
-            this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -162,6 +228,8 @@
             this.Controls.Add(this.label1);
             this.Name = "frmArtikliDodaj";
             this.Load += new System.EventHandler(this.frmArtikliDodaj_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,9 +245,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox tBoxDostavljenaKolicina;
+        protected System.Windows.Forms.Panel panel3;
+        protected System.Windows.Forms.Button btnAdd;
+        protected System.Windows.Forms.Panel panel1;
+        protected System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label6;
     }
 }
